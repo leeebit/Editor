d2541ea0740beb386ff5c8264d8490bc95f9ca0d
leeebit <ffn_bia@bk.ru>
2024-06-03 14:53:28 +0300

    Треугольник

--- Editor/Triangle.h
+++ Editor/Triangle.h
@@ -7,7 +7,7 @@ private:
     sf::ConvexShape triangleShape;
 
 public:
-    Triangle(const sf::Vector2f& position, float baseLength = 50.f, float height = 50.f); 
+    Triangle(const sf::Vector2f& position, float baseLength = 50.f, float height = 50.f);
     ~Triangle();
     void drawElements(sf::RenderWindow& window) override;
     void moveElements(float offsetX, float offsetY) override;
